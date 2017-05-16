98c222d9a355e52a295c83fff4c529e85d3de08f
Bejiol <bejiol93@gmail.com>
2017-05-16 22:15:17 +0200

    Maldita consulta de las narices

--- Raspy/Raspduino.py
+++ Raspy/Raspduino.py
@@ -2,66 +2,82 @@ import serial
 import os
 import MySQLdb
 
-# Información base de datos 
-DB_HOST = 'localhost' 
-DB_USER = 'root' 
-DB_PASS = '' # Completar!!
-DB_NAME = 'sed'
- 
-#Arduino con RFID y Teclado
-arduinoSensor = serial.Serial('/dev/ttyACM0', 9600)
-#Arduino con LCD, servo y zumbador
-arduinoActuador = serial.Serial('/dev/ttyACM1', 9600)
- 
-print("System ON. Waiting for inputs...")
- 
-continuar = True
-while continuar:
-	mensaje = arduinoSensor.readline()
-	# print(mensaje)
-
-	if mensaje.split(':')[0] == "UID :":
-                rfid_uid =  mensaje.split(':')[1]
-                res = consultaUsuario(rfid_uid)
-                arduinoSensor.write(res)
-                if res == False: # Usuario desconocido 
-                        arduinoActuador.write('n')
-                else: # Usuario conocido
-                        arduinoActuador.write('d')
-        elif mensaje == "key": # Pulsa tecla normal
-                arduinoActuador.write('k')
-        elif mensaje == "remove": # Pulsa tecla de borrado de clave
-                arduinoActuador.write('r')
-        elif mensaje == "correct": # La contraseña era correcta
-                arduinoActuador.write('u')
-        elif mensaje == "incorrect": # La contraseña no era correcta
-                arduinoActuador.write('w')
-        elif mensaje == "max_attempts":
-                arduinoActuador.write('m')
-	
-
-arduinoActuador.close() #Finalizamos la comunicacion
-arduinoSensor.close()
-
 def run_query(query): 
     datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME] 
     conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
     cursor = conn.cursor()         # Crear un cursor 
     cursor.execute(query)          # Ejecutar una consulta 
  
-    query.upper().startswith('SELECT'): 
-    data = cursor.fetchall()   # Traer los resultados de un select 
- 
+    query.upper().startswith('SELECT') 
+    data = cursor.fetchall()   # Traer los resultados de un select
+        
+    result = False     
+    for row in data:
+         result = row[0]
+
     cursor.close()                 # Cerrar el cursor 
     conn.close()                   # Cerrar la conexion 
+    return result    
  
-    return data
-
 def consultaUsuario(rfid):
-     result = run_query('SELECT codAcceso FROM usuarios WHERE codRFID=' + rfid)
-     if result == None:
-             res = False
-     else:
-             res = result[0]
-             
-     return res        
+     print(rfid)   
+     val = rfid.strip()
+     #print(val)
+     msg = "SELECT codAcceso FROM usuarios WHERE codRFID LIKE '%s'" % val
+     print(msg)
+     result = run_query(msg)
+     
+     if result == False:
+             result = "FALSE"
+        
+     return result        
+
+
+# Informacion base de datos 
+DB_HOST = 'localhost' 
+DB_USER = 'root' 
+DB_PASS = 'VillerKulla1993' # Completar!!
+DB_NAME = 'sed'
+ 
+#Arduino con RFID y Teclado
+arduinoSensor = serial.Serial('/dev/ttyACM0', 9600)
+#Arduino con LCD, servo y zumbador
+# arduinoActuador = serial.Serial('/dev/ttyACM1', 9600)
+ 
+print("System ON. Waiting for inputs...")
+ 
+continuar = True
+while continuar:
+	mensaje = arduinoSensor.readline()
+	print(mensaje)
+	if mensaje.split(':')[0] == "UID":
+                rfid_uid =  mensaje.split(':')[1]
+                res = consultaUsuario(rfid_uid)
+                print(res)
+                arduinoSensor.write(str(res))
+                """
+                if res == False:
+                        #arduinoActuador.write('n')
+                else:
+                        #arduinoActuador.write('d')
+                """
+        elif mensaje.strip() == "key": # Pulsa tecla normal
+               #arduinoActuador.write('k')
+                print('k')
+        elif mensaje.strip() == "remove": # Pulsa tecla de borrado de clave
+               #arduinoActuador.write('r')
+                print('r')
+        elif mensaje.strip() == "correct": # La contrasena era correcta
+               #arduinoActuador.write('u')
+                print('u')
+        elif mensaje.strip() == "incorrect": # La contrasena no era correcta
+               #arduinoActuador.write('w')
+                print('w')
+        elif mensaje.strip() == "max_attempts":
+               #arduinoActuador.write('m')
+                print('m')
+	
+
+#arduinoActuador.close() #Finalizamos la comunicacion
+arduinoSensor.close()
+
